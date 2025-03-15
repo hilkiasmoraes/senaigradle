8196b285b052f8a3a9080773ee642a1ef9f89660
Hilkias Moraes <hilkias.moraes@gmail.com>
2025-03-15 17:09:04 -0300

    feat: Spring boot projeto gradle

---
+++ src/main/java/com/senai/senaigradle/SenaigradleApplication.java
@@ -0,0 +1,13 @@
+package com.senai.senaigradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaigradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaigradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/senaigradle/SenaigradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.senaigradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaigradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
