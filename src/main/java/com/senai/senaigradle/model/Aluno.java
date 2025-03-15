b3186b16dd2639d5969d6fae8087c14b98ca7464
Hilkias Moraes <hilkias.moraes@gmail.com>
2025-03-15 17:38:00 -0300

    feat: Class

---
+++ src/main/java/com/senai/senaigradle/model/Aluno.java
@@ -0,0 +1,8 @@
+package com.senai.senaigradle.model;
+
+public class Aluno {
+    private int Id;
+    private String Nome;
+    private String CPF;
+    private String Email;
+}
---
+++ src/main/java/com/senai/senaigradle/model/Escola.java
@@ -0,0 +1,8 @@
+package com.senai.senaigradle.model;
+
+public class Escola {
+    private int Id;
+    private String Nome;
+    private String CNPJ;
+    private String Email;
+}
